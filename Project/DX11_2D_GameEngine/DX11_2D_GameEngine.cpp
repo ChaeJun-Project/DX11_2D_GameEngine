1717a475575da25c5a40786d1a3772edcaf32e68
chaezun <39609369+ChaeJun-Project@users.noreply.github.com>
2021-09-10 00:12:11 +0900

    2D 게임엔진 초기 설정(21/09/10)
    1. 정적 라이브러리 생성 및 연결
    2. 실행파일 생성 경로 설정(Debug, Release 모드)
    3. 윈도우창 관련 함수 모듈화

---
+++ Project/DX11_2D_GameEngine/DX11_2D_GameEngine.cpp
@@ -0,0 +1,20 @@
+﻿#include "stdafx.h"
+#include "Window.h"
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                      _In_opt_ HINSTANCE hPrevInstance,
+                      _In_ LPWSTR    lpCmdLine,
+                      _In_ int       nCmdShow)
+{
+    Window::Create(hInstance, 1200, 800, L"DX11_2D_GameEngine");
+    Window::Show(nCmdShow);
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_DX112DFRAMEWORK));
+
+    while (Window::Update(hAccelTable))
+    {
+       //게임 로직넣기
+    }
+
+    return 0;
+}
---
+++ Project/DX11_2D_GameEngine/unity_RV0MYTO7D50JFWGQ.cpp
@@ -0,0 +1,4 @@
+#include "stdafx.h"
+
+#include "C:\Users\User\Desktop\DX11_2D_GameEngine\Project\DX11_2D_GameEngine\DX11_2D_GameEngine.cpp"
+
